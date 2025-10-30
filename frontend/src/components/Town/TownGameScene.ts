a99f7ff2a69dce21395d5adf0377952b412e8c24
Dereck Vasquez <dereckve2004@gmail.com>
2025-10-30 13:32:26 -0400

    fixed bug that would make other player sprites appear under map object

--- frontend/src/components/Town/TownGameScene.ts
+++ frontend/src/components/Town/TownGameScene.ts
@@ -293,6 +293,18 @@ export default class TownGameScene extends Phaser.Scene {
           player.gameObjects.label.setY(player.gameObjects.sprite.body.y - 20);
         }
       }
+
+      const allPlayers = [...this._players, this.coveyTownController.ourPlayer];
+      allPlayers.forEach(p => {
+        const s = p.gameObjects?.sprite;
+        const l = p.gameObjects?.label;
+        if (s) {
+          s.setDepth(s.y);        // player depth
+          if (l) {
+            l.setDepth(s.y + 1);  // label slightly above the player
+          }
+        }
+      });
     }
   }
 
